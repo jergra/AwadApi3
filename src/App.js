b13e61ab7f65ebdead342ce93f22509cf6e1d7a8
jergra <jergra43@gmail.com>
2021-07-05 16:17:57 -0700

    colors

--- src/App.js
+++ src/App.js
@@ -2,7 +2,24 @@ import React, {useEffect, useState} from 'react'
 import axios from 'axios'
 import './App.css';
 
-  
+const getColor = (colorInt, part) => {
+  console.log("part:", part)
+  console.log("colorInt:", colorInt)
+  var color = ''
+  if (colorInt === 10) {
+    color = '#a' + part
+    console.log("color:", color)
+  }
+  if (colorInt === 11) {
+    color = '#b' + part
+    console.log("color:", color)
+  }
+  if (colorInt < 10) {
+    color = '#' + colorInt  + part
+    console.log("color:", color)
+  }
+  return color
+}
 const fetchData = () => {
   return axios.get('https://randomuser.me/api/?results=12')
   .then((res) => {
@@ -19,10 +36,26 @@ export default function App() {
   
   const [people, setPeople] = useState([])
   const [stuff, setStuff] = useState([])
+  const [part, setPart] = useState()
+
 
   useEffect(() => {
     fetchData().then(apiPeople => {
       setPeople(apiPeople)
+      var randomPart = Math.floor(Math.random() * 9).toString() 
+                      + Math.floor(Math.random() * 6).toString()
+      for (var k=0;k<3;k++) {
+        var randomNumber = Math.floor(Math.random() * 16)
+        if (randomNumber < 10) randomPart = randomPart + randomNumber.toString()
+        if (randomNumber === 10) randomPart = randomPart + 'a'
+        if (randomNumber === 11) randomPart = randomPart + 'b'
+        if (randomNumber === 12) randomPart = randomPart + 'c'
+        if (randomNumber === 13) randomPart = randomPart + 'd'
+        if (randomNumber === 14) randomPart = randomPart + 'e'
+        if (randomNumber === 15) randomPart = randomPart + 'f'
+      }
+      console.log("randomPart:", randomPart)
+      setPart(randomPart)
     })
   }, [])
 
@@ -32,8 +65,18 @@ export default function App() {
     console.log("BUTTON CLICKED 1")
     console.log("people 1: ", people)
     let stuff = []
+    let countries = []
+
+    for (var j=0;j<people.length;j++) {
+      countries.push(people[j].location.country + people[j].name.first)
+    }
+    countries.sort()
+    countries.reverse()
+    console.log("countries:", countries)
   
     for (var i=0;i<people.length;i++) {
+      var countryIndex = countries.indexOf(people[i].location.country + people[i].name.first);
+      console.log("country:", people[i].location.country, "countryIndex:", countryIndex)
       //if (people[i].location.country === 'Canada') {
       stuff.push([people[i].location.country,
                     people[i].name.first, 
@@ -42,12 +85,15 @@ export default function App() {
                     people[i].location.street.name,
                     people[i].location.city,
                     people[i].location.state,
-                    people[i].picture.large])
+                    people[i].picture.large,
+                    countryIndex
+                  ])
       //}
     }
     console.log("stuff 1: ", stuff)
     setStuff(stuff)
     console.log("stuff 2: ", stuff)
+    
   }
 
   const doThis = () => {
@@ -69,19 +115,63 @@ export default function App() {
       <button onClick={refresh}>Clear</button>
     <div className="App">
     
-      <div className="block">
+      <div className="block1">
         
         <div className="row6">
           {stuff.slice(0,6).map((person, personIdx) => 
-          <div>
+          <div className="individual" style={{backgroundColor: getColor(person[8], part)}}>
+          {/* <div className="individual" style={{backgroundColor: '#9'}}> */}
             <div className="entry6" key={personIdx}>
               <img src={person[7]} />
             </div>
+            <div className="entry2" key={personIdx}>
+              {person[1]}&nbsp;{person[2]}
+            </div>
+            <div>
+              <div className="entry3" key={personIdx}>{person[3]} &nbsp;{person[4]}</div>
+            </div>
+            <div>
+              <div className="entry" key={personIdx}>{person[5]}</div>
+            </div>
+            <div>
+              <div className="entry" key={personIdx}>{person[6]}</div>
+            </div>
+            <div>
+              <div className="entry" key={personIdx}>{person[0]}</div>
+            </div>
+            <div>{getColor(person[8], part)}</div>
+          </div>
+          )}
+        </div>
+      </div>
+      <div className="block2">
+        <div className="row6">
+          {stuff.slice(6,12).map((person, personIdx) => 
+          <div className="individual" style={{backgroundColor: getColor(person[8], part)}}>
+            <div className="entry6" key={personIdx}>
+              <img src={person[7]} />
+            </div>
+            <div className="entry2" key={personIdx}>
+              {person[1]}&nbsp;{person[2]}
+            </div>
+            <div>
+              <div className="entry3" key={personIdx}>{person[3]} &nbsp;{person[4]}</div>
+            </div>
+            <div>
+              <div className="entry" key={personIdx}>{person[5]}</div>
+            </div>
+            <div>
+              <div className="entry" key={personIdx}>{person[6]}</div>
+            </div>
+            <div>
+              <div className="entry" key={personIdx}>{person[0]}</div>
+            </div>
+            <div>{getColor(person[8], part)}</div>
           </div>
           )}
         </div>
 
-        <div className="row1">
+        {/* <div className="row1">
           {stuff.slice(0,6).map((person, personIdx) => 
           <div>
             <div className="entry2" key={personIdx}>
@@ -176,7 +266,7 @@ export default function App() {
           </div>
           )}
         </div>
-        
+         */}
       </div> 
         
     </div>
